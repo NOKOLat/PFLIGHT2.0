178a7eb6f9ecabc3ec5f6e2862d1241003c40060
aoi-256 <aoi256jp@Gmail.com>
2026-04-20 17:43:14 +0900

    icm42688pの実装

--- Core/StateManager/common/state_context.hpp
+++ Core/StateManager/common/state_context.hpp
@@ -3,11 +3,14 @@
 
 #include <cstdint>
 #include <functional>
+#include <optional>
 #include <string>
 #include "stm32f7xx_hal.h"
 #include "SBUS/sbus.h"
 #include "sbus_rescaler.hpp"
 #include "usart.h"
+#include "gpio.h"
+#include "ICM42688P/ICM42688P.h"
 
 struct StateContext {
 
@@ -19,6 +22,9 @@ struct StateContext {
     UART_HandleTypeDef*      sbus_uart  = &huart5;
     UART_HandleTypeDef*      debug_sbus_uart  = &huart2; // デバッグ用（必要に応じて変更）
     nokolat::RescaledSBUSData sbus_data = {};
+
+    // ICM42688P（遅延初期化: InitStateでemplace）
+    std::optional<ICM42688P> imu = std::nullopt;
 };
 
 
