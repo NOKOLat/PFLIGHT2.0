1c7c9d5898d86744cfd98a10bc633f8bfe277475
aoi-netai <aoi256jp@Gmail.com>
2026-04-21 01:00:05 +0900

    遷移する場所でStateChange::Nochangeになっている問題を修正

--- Core/StateManager/State/PreArm/pre_flight_state.cpp
+++ Core/StateManager/State/PreArm/pre_flight_state.cpp
@@ -16,6 +16,11 @@ StateError PreFlightState::update(StateContext& context) {
 
 StateResult PreFlightState::evaluateNextState(StateContext& context) {
 
+	if(context.sbus_data.arm !=  SwitchPosition::HIGH){
+
+		return {StateChange::STATE_CHANGE, StateID::PRE_ARM};
+	}
+
     // Safetyスイッチが有効になったらFlightStateへ遷移
     if(context.sbus_data.safety == SwitchPosition::HIGH){
 
